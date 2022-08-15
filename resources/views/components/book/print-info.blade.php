57fd14a4fb30c94505f92dff744a48f161dad9e0
Natiztm <Natiztmgm@gmail.com>
2022-08-16 01:57:23 +0300

    book print Module 1. list print order view 2. new print info blade component

---
+++ resources/views/components/book/print-info.blade.php
@@ -0,0 +1,25 @@
+@props(['batch', 'copies', 'packages'])
+<div class="flex items-center">
+    <div class="text-sm text-gray-500 font-semibold text-left">
+        <div class="">
+            Batch No.:
+        </div>
+        <div class="">
+            No. Copies:
+        </div>
+        <div class="">
+            No. Packages:
+        </div>
+    </div>
+    <div class="ml-3 text-sm font-medium text-gray-700">
+        <div class="">
+            {{ $batch }}
+        </div>
+        <div class="">
+            {{ $copies }}
+        </div>
+        <div class="">
+            {{ $packages }}
+        </div>
+    </div>
+</div>
