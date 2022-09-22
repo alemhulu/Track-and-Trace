501a8b78dbf4b12d546c6d4d8bf41bc5d7ee25b5
Natiztm <Natiztmgm@gmail.com>
2022-09-22 21:13:44 +0300

    Package Module 1. new book-package route 2. new book-package view index 3. new book-package livewire component 4. add new side nav blade component 5. add new side footer blade component

---
+++ resources/views/components/side-navigation/footer.blade.php
@@ -0,0 +1,3 @@
+<div class="py-2 border-none horizontal-btn-active dark:bg-gray-700">
+    <p class="text-xs text-blue-800/70 text-center tracking-wider dark:text-gray-400">Version: 2.23</p>
+</div>
---
+++ resources/views/components/side-navigation/nav.blade.php
@@ -0,0 +1,43 @@
+<div class="mt-5 flex-1 flex flex-col">
+    <nav class="flex-1 px-2 pb-4 space-y-1">
+        <x-side-navigation.link href="/dashboard" icon="fi-rr-dashboard" name="Dashboard"
+            :active="request()->routeIs('dashboard')" />
+
+        <x-side-navigation.link href="{{ route('users.index') }}" icon="fi-rr-user" name="Users"
+            :active="request()->routeIs('users.*')" />
+
+        <x-side-navigation.link href="{{ route('roles.index') }}" icon="fi-rr-key" name="Roles"
+            :active="request()->routeIs('roles.*')" />
+
+        <x-side-navigation.link href="/location" icon="fi-rr-map-marker-home" name="Location"
+            :active="request()->routeIs('location')" />
+
+        <x-side-navigation.link href="/organization" icon="fi-rr-building " name="Organization"
+            :active="request()->routeIs('organization')" />
+
+        <x-side-navigation.link href="/print-order" icon="fi-rr-print" name="Print Order"
+            :active="request()->routeIs('print-order')" />
+
+        <x-side-navigation.link href="/book-packages" icon="fi-rr-box" name="Packages"
+            :active="request()->routeIs('book-packages')" />
+
+        <x-side-navigation.link href="/warehouse" icon="fi-rr-home-location-alt " name="Warehouse"
+            :active="request()->routeIs('warehouse')" />
+
+        <x-side-navigation.link href="/book" icon="fi-rr-book" name="Book" :active="request()->routeIs('book')" />
+
+        <x-side-navigation.link href="/route" icon="fi-rr-chart-tree" name="Route"
+            :active="request()->routeIs('route')" />
+
+        <x-side-navigation.link href="/distribution" icon="fi-rr-truck-side" name="Distribution"
+            :active="request()->routeIs('distribution')" />
+
+        <x-side-navigation.link href="/trace" icon="fi-rr-paw" name="Trace"
+            :active="( request()->routeIs('trace') | request()->routeIs('distribution-details.*'))" />
+
+        @can('view-logs')
+        <x-side-navigation.link href="/log-viewer" icon="fi-rr-info" name="LOG"
+            :active="request()->routeIs('log-viewer')" />
+        @endcan
+    </nav>
+</div>
