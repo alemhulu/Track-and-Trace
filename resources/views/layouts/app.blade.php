c7756c8e014125de06bd9792239fd3a76a9eee93
Natiztm <Natiztmgm@gmail.com>
2022-10-27 18:26:08 +0300

    add tast env

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,7 +7,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
-        <link rel="icon" class="bg-white" href="https://moe.gov.et/logom.png">
+        <link rel="icon" href="https://moe.gov.et/logom.png">
         <title>{{ config('app.name', 'Track and Trace') }}</title>
 
         <!-- Fonts -->
