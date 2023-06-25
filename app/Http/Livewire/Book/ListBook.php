4c402df026a74052ce1650d28df8b3427678c507
alemhulu <alemhulu@gmail.com>
2023-06-25 21:32:50 +0300

    book list view

--- app/Http/Livewire/Book/ListBook.php
+++ app/Http/Livewire/Book/ListBook.php
@@ -2,12 +2,29 @@
 
 namespace App\Http\Livewire\Book;
 
+use App\Models\Book;
 use Livewire\Component;
-
+use Livewire\WithPagination;
 class ListBook extends Component
-{
+{ 
+    use WithPagination;
+    //variables
+    public $books=[];
+    public $column, $recordes, $sortType;
+    public function mount()
+    {
+        $this->books=Book::all();
+        $this->recordes=5;
+        $this->column='';
+    }
     public function render()
     {
-        return view('livewire.book.list-book')->extends('main.book.index');
+        return view('livewire.book.list-book',
+        ['books'=>Book::
+        when($this->column,function($q,$column){
+            return $q->orderBy($this->column,$this->sortType);
+        })->paginate($this->recordes)
+
+    ])->extends('main.book.index');
     }
 }
--- resources/views/livewire/book/list-book.blade.php
+++ resources/views/livewire/book/list-book.blade.php
@@ -13,44 +13,15 @@
             @php $i = 1; $record = 1;@endphp
             {{-- @forelse($books as $record) --}}
             <x-data-table.tr>
+                @foreach ($books as $book)
                 <td class="px-5 py-2 whitespace-nowrap">
-                    <div class="text-sm text-gray-700 dark:text-gray-100">1</div>
+                    <div class="text-sm text-gray-700 dark:text-gray-100">{{ $book->id }}</div>
                 </td>
 
                 <td class="px-5 py-2 whitespace-nowrap">
-                    <x-book.book-info image="/biology-grade-10.jpg" grade="Grade 10" subject="Biology"
-                        type="Student Text Book" edition="1st Edition 2013" ISBN="4820715" />
-                </td>
-
-                <td class="px-5 py-2 whitespace-nowrap">
-                    <x-book.book-standards font="Noto Sans Ethiopics" print="Color Print (RGB)" paper="A5" />
-                </td>
-
-                <td class="px-5 py-2 whitespace-nowrap">
-                    <x-book.book-file name="Grade-10-Student-Text-Book-Biology" type="PDF" size="15MB" />
-                </td>
-
-                <td class="px-5 py-2 whitespace-nowrap">
-                    <div class="text-md font-semibold text-gray-500 dark:text-gray-300">
-                        14,2013 copies
-                    </div>
-                </td>
-
-                <td class="px-5 py-2">
-                    <x-action.table-button id="{{ $record }}" view="viewBook" edit="editBook" delete="deleteBook" />
-                </td>
-            </x-data-table.tr>
-
-            <x-data-table.tr>
-                <td class="px-5 py-2 whitespace-nowrap">
-                    <div class="text-sm text-gray-700 dark:text-gray-100">2</div>
-                </td>
-
-                <td class="px-5 py-2 whitespace-nowrap">
-                    <x-book.book-info
-                        image="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTO4PF0UBFIlAShsDV_dJAqaXPUMuSHLAT-bcp6nyF52wSILxHhmiXOjB2ZYb8Vri76pbI&usqp=CAU"
-                        grade="Grade 12" subject="Physics" type="Student Text Book" edition="2nd Edition 2011"
-                        ISBN="4820715" />
+                    <x-book.book-info image="{{$book->front_cover_location }}" grade="{{ $book->grade->name }}"
+                        subject="{{ $book->subject->name }}" type="Student Text Book"
+                        edition="Edition {{ $book->edition }}" ISBN="{{ $book->isbn }}" />
                 </td>
 
                 <td class="px-5 py-2 whitespace-nowrap">
@@ -58,19 +29,24 @@
                 </td>
 
                 <td class="px-5 py-2 whitespace-nowrap">
-                    <x-book.book-file name="Grade-12-Student-Text-Book-Physics" type="PDF" size="17MB" />
+                    <x-book.book-file name="GRADE {{ $book->grade->name }}  {{ $book->subject->name }}" type="PDF"
+                        size="15MB" />
                 </td>
 
                 <td class="px-5 py-2 whitespace-nowrap">
                     <div class="text-md font-semibold text-gray-500 dark:text-gray-300">
-                        50,500 copies
+                        --
                     </div>
                 </td>
 
                 <td class="px-5 py-2">
                     <x-action.table-button id="{{ $record }}" view="viewBook" edit="editBook" delete="deleteBook" />
                 </td>
+                @endforeach
+
             </x-data-table.tr>
+
+
             {{-- @empty --}}
             {{-- <x-data-table.empty colspan=6 /> --}}
             {{-- @endforelse --}}
